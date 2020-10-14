7e26d44f5bdc6a1df30388e0e5cf9b9d2fdfe7bf
simplydahlia <nicetomeetyou@live.cl>
2020-10-14 17:05:40 -0300

    Initial Commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export interface IHomeProps {
+
+}
+
+const Home: React.SFC<IHomeProps> = (props) => {
+    return (
+        <div>
+            <h1>
+                Hola
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
